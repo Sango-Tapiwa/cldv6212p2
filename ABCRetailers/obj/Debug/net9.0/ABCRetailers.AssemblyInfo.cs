aefade57bfdd2895fa23855612460bb252a9a5e9
Sango-Tapiwa <st10276025@imconnect.edu.za>
2025-10-08 23:00:15 +0200

    4th
    Connection string removed

--- ABCRetailers/obj/Debug/net9.0/ABCRetailers.AssemblyInfo.cs
+++ ABCRetailers/obj/Debug/net9.0/ABCRetailers.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ABCRetailers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+875bb774ea5e0eae564b5b651959e85a9cf140c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ABCRetailers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ABCRetailers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
